00440d8c336a6a0d7a5f78beb23d34f5403adb80
VarodinAleksandr <olisgroup21@gmail.com>
2022-10-07 23:20:47 +0300

    hw_03

---
+++ flaskr1/init.sql
@@ -0,0 +1,13 @@
+INSERT INTO tracks (title, artist, genre, length_track)
+VALUES
+  ('sonata', 'Bah', 'classic', 250),
+  ('tokata', 'Johan', 'classic-modern', 300),
+  ('hard_song', 'Sebastian', 'rock', 200),
+  ('tears', 'Roger', 'pop', 300),
+  ('fly', 'Tiesto', 'techno', 250),
+  ('sonata1', 'Bah', 'classic', 250),
+  ('tokata1', 'Johan', 'classic-modern', 300),
+  ('hard_song1', 'Sebastian', 'rock', 200),
+  ('tears1', 'Roger', 'pop', 300),
+  ('fly1', 'Tiesto', 'techno', 250);
+
